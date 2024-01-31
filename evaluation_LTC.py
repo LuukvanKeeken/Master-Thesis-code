b7fde2164f7608d932ea8ed64282533dc7d2d4c4
Luuk van Keeken <luukvankeeken24@gmail.com>
2024-01-31 20:20:26 +0100

    Created evaluation script for LTC.

--- evaluation_LTC.py
+++ evaluation_LTC.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np
 from LTC_A2C import LTC_Network, CfC_Network
 import torch
@@ -38,9 +39,69 @@ def evaluate_LTC_agent_pole_length(agent_net, env_name, num_episodes, evaluation
 
     return eval_rewards
 
+def evaluate_LTC_agent_pole_mass(agent_net, env_name, num_episodes, evaluation_seeds, pole_mass_modifier):
 
+    eval_rewards = []
+    env = gym.make(env_name)
+    env.unwrapped.masspole *= pole_mass_modifier
+        
+    for i_episode in range(num_episodes):
+        hidden_state = None
+        
+        env.seed(int(evaluation_seeds[i_episode]))
+        
+        state = env.reset()
+        total_reward = 0
+        done = False
 
+        while not done:
+            state = torch.from_numpy(state)
+            state = state.unsqueeze(0).to(device) #This as well?
+            policy_output, value, hidden_state = agent_net(state.float(), hidden_state)
+            
+            policy_dist = torch.softmax(policy_output, dim = 1)
+            
+            action = torch.argmax(policy_dist)
+            
 
+            state, r, done, _ = env.step(action.item())
+
+            total_reward += r
+        eval_rewards.append(total_reward)
+
+    return eval_rewards
+
+def evaluate_LTC_agent_force_mag(agent_net, env_name, num_episodes, evaluation_seeds, force_mag_modifier):
+
+    eval_rewards = []
+    env = gym.make(env_name)
+    env.unwrapped.force_mag *= force_mag_modifier
+        
+    for i_episode in range(num_episodes):
+        hidden_state = None
+        
+        env.seed(int(evaluation_seeds[i_episode]))
+        
+        state = env.reset()
+        total_reward = 0
+        done = False
+
+        while not done:
+            state = torch.from_numpy(state)
+            state = state.unsqueeze(0).to(device) #This as well?
+            policy_output, value, hidden_state = agent_net(state.float(), hidden_state)
+            
+            policy_dist = torch.softmax(policy_output, dim = 1)
+            
+            action = torch.argmax(policy_dist)
+            
+
+            state, r, done, _ = env.step(action.item())
+
+            total_reward += r
+        eval_rewards.append(total_reward)
+
+    return eval_rewards
 
 
 
@@ -48,35 +109,170 @@ env_name = "CartPole-v0"
 max_reward = 200
 max_steps = 200
 n_evaluations = 100
+neuron_type = "LTC"
 
 evaluation_seeds = np.load('rstdp_cartpole_stuff/seeds/evaluation_seeds.npy')
 
+results_dir = f"LTC_a2c_result_7_2024130_learningrate_0.0001_selectiomethod_evaluation_gamma_0.99_trainingmethod_standard"
+os.mkdir(f"LTC_A2C/evaluation_results/{results_dir}")
 
-weights_0 = torch.load('LTC_A2C/training_results/CfC_a2c_result_7_2024130_learningrate_0.0001_selectiomethod_evaluation_gamma_0.99_trainingmethod_standard/checkpoint_CfC_A2C_0.pt', map_location=torch.device(device))
-weights_1 = torch.load('LTC_A2C/training_results/CfC_a2c_result_7_2024130_learningrate_0.0001_selectiomethod_evaluation_gamma_0.99_trainingmethod_standard/checkpoint_CfC_A2C_1.pt', map_location=torch.device(device))
-weights_2 = torch.load('LTC_A2C/training_results/CfC_a2c_result_7_2024130_learningrate_0.0001_selectiomethod_evaluation_gamma_0.99_trainingmethod_standard/checkpoint_CfC_A2C_2.pt', map_location=torch.device(device))
-weights_3 = torch.load('LTC_A2C/training_results/CfC_a2c_result_7_2024130_learningrate_0.0001_selectiomethod_evaluation_gamma_0.99_trainingmethod_standard/checkpoint_CfC_A2C_3.pt', map_location=torch.device(device))
-weights_4 = torch.load('LTC_A2C/training_results/CfC_a2c_result_7_2024130_learningrate_0.0001_selectiomethod_evaluation_gamma_0.99_trainingmethod_standard/checkpoint_CfC_A2C_4.pt', map_location=torch.device(device))
-weights_5 = torch.load('LTC_A2C/training_results/CfC_a2c_result_7_2024130_learningrate_0.0001_selectiomethod_evaluation_gamma_0.99_trainingmethod_standard/checkpoint_CfC_A2C_5.pt', map_location=torch.device(device))
-weights_6 = torch.load('LTC_A2C/training_results/CfC_a2c_result_7_2024130_learningrate_0.0001_selectiomethod_evaluation_gamma_0.99_trainingmethod_standard/checkpoint_CfC_A2C_6.pt', map_location=torch.device(device))
-weights_7 = torch.load('LTC_A2C/training_results/CfC_a2c_result_7_2024130_learningrate_0.0001_selectiomethod_evaluation_gamma_0.99_trainingmethod_standard/checkpoint_CfC_A2C_7.pt', map_location=torch.device(device))
-weights_8 = torch.load('LTC_A2C/training_results/CfC_a2c_result_7_2024130_learningrate_0.0001_selectiomethod_evaluation_gamma_0.99_trainingmethod_standard/checkpoint_CfC_A2C_8.pt', map_location=torch.device(device))
-weights_9 = torch.load('LTC_A2C/training_results/CfC_a2c_result_7_2024130_learningrate_0.0001_selectiomethod_evaluation_gamma_0.99_trainingmethod_standard/checkpoint_CfC_A2C_9.pt', map_location=torch.device(device))
+
+weights_0 = torch.load(f'LTC_A2C/training_results/{results_dir}/checkpoint_{neuron_type}_A2C_0.pt', map_location=torch.device(device))
+weights_1 = torch.load(f'LTC_A2C/training_results/{results_dir}/checkpoint_{neuron_type}_A2C_1.pt', map_location=torch.device(device))
+weights_2 = torch.load(f'LTC_A2C/training_results/{results_dir}/checkpoint_{neuron_type}_A2C_2.pt', map_location=torch.device(device))
+weights_3 = torch.load(f'LTC_A2C/training_results/{results_dir}/checkpoint_{neuron_type}_A2C_3.pt', map_location=torch.device(device))
+weights_4 = torch.load(f'LTC_A2C/training_results/{results_dir}/checkpoint_{neuron_type}_A2C_4.pt', map_location=torch.device(device))
+weights_5 = torch.load(f'LTC_A2C/training_results/{results_dir}/checkpoint_{neuron_type}_A2C_5.pt', map_location=torch.device(device))
+weights_6 = torch.load(f'LTC_A2C/training_results/{results_dir}/checkpoint_{neuron_type}_A2C_6.pt', map_location=torch.device(device))
+weights_7 = torch.load(f'LTC_A2C/training_results/{results_dir}/checkpoint_{neuron_type}_A2C_7.pt', map_location=torch.device(device))
+weights_8 = torch.load(f'LTC_A2C/training_results/{results_dir}/checkpoint_{neuron_type}_A2C_8.pt', map_location=torch.device(device))
+weights_9 = torch.load(f'LTC_A2C/training_results/{results_dir}/checkpoint_{neuron_type}_A2C_9.pt', map_location=torch.device(device))
 weights = [weights_0, weights_1, weights_2, weights_3, weights_4, weights_5, weights_6, weights_7, weights_8, weights_9]
 
-eraser = '\b \b'
+
+# ORIGINAL ENVIRONMENT EVALUATION ---------------------------
+with open(f"LTC_A2C/evaluation_results/{results_dir}/original_env_evals.txt", "w") as f:
+    eraser = '\b \b'
+    original_eval_rewards = []
+    for i, w in enumerate(weights):
+        print('Run {:02d} ...'.format(i), end='')
+        if neuron_type == "CfC":
+            agent_net = CfC_Network(4, 64, 2, 5).to(device)
+        elif neuron_type == "LTC":
+            agent_net = LTC_Network(4, 64, 2, 5).to(device)
+        
+        agent_net.load_state_dict(w)
+
+        rewards = evaluate_LTC_agent_pole_length(agent_net, env_name, n_evaluations, evaluation_seeds, 1.0)
+        original_eval_rewards.append(rewards)
+        print(eraser*3 + '-> Avg reward: {:7.2f}'.format(np.mean(rewards)))
+        f.write(f"Run {i}: avg reward: {np.mean(rewards)}\n")
+
+    print(f"Mean avg reward: {np.mean(np.mean(original_eval_rewards, axis = 1))}, +/- {np.std(np.mean(original_eval_rewards, axis = 1))}")
+    f.write(f"Mean avg reward: {np.mean(np.mean(original_eval_rewards, axis = 1))}, +/- {np.std(np.mean(original_eval_rewards, axis = 1))}\n")
 
 
-original_eval_rewards = []
-for i, w in enumerate(weights):
-    print(f'Run #{i}', end='')
-    agent_net = CfC_Network(4, 64, 2, 5).to(device)
-    agent_net.load_state_dict(w)
 
-    rewards = evaluate_LTC_agent_pole_length(agent_net, env_name, n_evaluations, evaluation_seeds, 1.0)
-    original_eval_rewards.append(rewards)
-    print(eraser*3 + '-> Avg reward: {:7.2f}'.format(np.mean(rewards)))
+# POLE LENGTH EVALUATION ---------------------------
+percentages = np.linspace(0.1, 2.0, 20)
+percentages = np.concatenate((percentages, np.linspace(2.5, 20, 36)))
+all_modified_env_eval_rewards = []
+for percentage in percentages:
+    print(percentage)
+    modified_env_eval_rewards = []
 
-print(f"Mean avg reward: {np.mean(original_eval_rewards)}")
+    for i, w in enumerate(weights):
+        print('Run {:02d} ...'.format(i), end='')
+        if neuron_type == "CfC":
+            agent_net = CfC_Network(4, 64, 2, 5).to(device)
+        elif neuron_type == "LTC":
+            agent_net = LTC_Network(4, 64, 2, 5).to(device)
+        agent_net.load_state_dict(w)
+
+        rewards = evaluate_LTC_agent_pole_length(agent_net, env_name, n_evaluations, evaluation_seeds, percentage)
+        modified_env_eval_rewards.append(rewards)
+
+        print(eraser*3 + '-> Avg reward: {:7.2f}'.format(np.mean(rewards)))
+
+    all_modified_env_eval_rewards.append(modified_env_eval_rewards)
+
+mean_avgs = []
+std_dev_avgs = []
+median_avgs = []
+for results in all_modified_env_eval_rewards:
+    means_per_model = np.mean(results, axis = 1)
+    mean_avgs.append(np.mean(means_per_model))
+    std_dev_avgs.append(np.std(means_per_model))
+    median_avgs.append(np.median(means_per_model))
+
+
+os.mkdir(f"LTC_A2C/evaluation_results/{results_dir}/pole_length")
+np.save(f"LTC_A2C/evaluation_results/{results_dir}/pole_length/means.npy", mean_avgs)
+np.save(f"LTC_A2C/evaluation_results/{results_dir}/pole_length/stddevs.npy", std_dev_avgs)
+np.save(f"LTC_A2C/evaluation_results/{results_dir}/pole_length/medians.npy", median_avgs)
+np.save(f"LTC_A2C/evaluation_results/{results_dir}/pole_length/percentages.npy", percentages)
+
+
+
+# POLE MASS EVALUATION ---------------------------
+percentages = np.linspace(5.0, 20.0, 16)
+all_modified_env_eval_rewards = []
+for percentage in percentages:
+    print(percentage)
+    modified_env_eval_rewards = []
+
+    for i, w in enumerate(weights):
+        print('Run {:02d} ...'.format(i), end='')
+        if neuron_type == "CfC":
+            agent_net = CfC_Network(4, 64, 2, 5).to(device)
+        elif neuron_type == "LTC":
+            agent_net = LTC_Network(4, 64, 2, 5).to(device)
+        agent_net.load_state_dict(w)
+
+        rewards = evaluate_LTC_agent_pole_mass(agent_net, env_name, n_evaluations, evaluation_seeds, percentage)
+        modified_env_eval_rewards.append(rewards)
+
+        print(eraser*3 + '-> Avg reward: {:7.2f}'.format(np.mean(rewards)))
+
+    all_modified_env_eval_rewards.append(modified_env_eval_rewards)
+
+mean_avgs = []
+std_dev_avgs = []
+median_avgs = []
+for results in all_modified_env_eval_rewards:
+    means_per_model = np.mean(results, axis = 1)
+    mean_avgs.append(np.mean(means_per_model))
+    std_dev_avgs.append(np.std(means_per_model))
+    median_avgs.append(np.median(means_per_model))
+
+
+os.mkdir(f"LTC_A2C/evaluation_results/{results_dir}/pole_mass")
+np.save(f"LTC_A2C/evaluation_results/{results_dir}/pole_mass/means.npy", mean_avgs)
+np.save(f"LTC_A2C/evaluation_results/{results_dir}/pole_mass/stddevs.npy", std_dev_avgs)
+np.save(f"LTC_A2C/evaluation_results/{results_dir}/pole_mass/medians.npy", median_avgs)
+np.save(f"LTC_A2C/evaluation_results/{results_dir}/pole_mass/percentages.npy", percentages)
+
+
+
+
+# FORCE MAG EVALUATION ---------------------------
+percentages = np.concatenate((np.linspace(0.2, 2.0, 19), np.linspace(2.5, 6.0, 8)))
+all_modified_env_eval_rewards = []
+for percentage in percentages:
+    print(percentage)
+    modified_env_eval_rewards = []
+
+    for i, w in enumerate(weights):
+        print('Run {:02d} ...'.format(i), end='')
+        if neuron_type == "CfC":
+            agent_net = CfC_Network(4, 64, 2, 5).to(device)
+        elif neuron_type == "LTC":
+            agent_net = LTC_Network(4, 64, 2, 5).to(device)
+        agent_net.load_state_dict(w)
+
+        rewards = evaluate_LTC_agent_force_mag(agent_net, env_name, n_evaluations, evaluation_seeds, percentage)
+        modified_env_eval_rewards.append(rewards)
+
+        print(eraser*3 + '-> Avg reward: {:7.2f}'.format(np.mean(rewards)))
+
+    all_modified_env_eval_rewards.append(modified_env_eval_rewards)
+
+mean_avgs = []
+std_dev_avgs = []
+median_avgs = []
+for results in all_modified_env_eval_rewards:
+    means_per_model = np.mean(results, axis = 1)
+    mean_avgs.append(np.mean(means_per_model))
+    std_dev_avgs.append(np.std(means_per_model))
+    median_avgs.append(np.median(means_per_model))
+
+
+os.mkdir(f"LTC_A2C/evaluation_results/{results_dir}/force_mag")
+np.save(f"LTC_A2C/evaluation_results/{results_dir}/force_mag/means.npy", mean_avgs)
+np.save(f"LTC_A2C/evaluation_results/{results_dir}/force_mag/stddevs.npy", std_dev_avgs)
+np.save(f"LTC_A2C/evaluation_results/{results_dir}/force_mag/medians.npy", median_avgs)
+np.save(f"LTC_A2C/evaluation_results/{results_dir}/force_mag/percentages.npy", percentages)
+
+
 
 
--- plotting/plot_evaluation.py
+++ plotting/plot_evaluation.py
@@ -6,53 +6,63 @@ fig, ax = plt.subplots()
 
 start = 0
 end = 200
-modified_parameter = "pole_length"
-statistic = "median"
+modified_parameter = "pole_mass"
+statistic = "mean"
 
-BP_evaluation_statistic = np.load(f"../BP_A2C/evaluation_results/BP_A2C_RNN_10models/{modified_parameter}/{statistic}s.npy")
-BP_evaluation_std = np.load(f"../BP_A2C/evaluation_results/BP_A2C_RNN_10models/{modified_parameter}/stddevs.npy")
-BP_evaluation_percentages = np.load(f"../BP_A2C/evaluation_results/BP_A2C_RNN_10models/{modified_parameter}/percentages.npy")
-BP_evaluation_percentages = (BP_evaluation_percentages*100)-100
+CfC_evaluation_statistic = np.load(f"../LTC_A2C/evaluation_results/CfC_a2c_result_7_2024130_learningrate_0.0001_selectiomethod_evaluation_gamma_0.99_trainingmethod_standard/{modified_parameter}/{statistic}s.npy")
+CfC_evaluation_std = np.load(f"../LTC_A2C/evaluation_results/CfC_a2c_result_7_2024130_learningrate_0.0001_selectiomethod_evaluation_gamma_0.99_trainingmethod_standard/{modified_parameter}/stddevs.npy")
+CfC_evaluation_percentages = np.load(f"../LTC_A2C/evaluation_results/CfC_a2c_result_7_2024130_learningrate_0.0001_selectiomethod_evaluation_gamma_0.99_trainingmethod_standard/{modified_parameter}/percentages.npy")
+CfC_evaluation_percentages = (CfC_evaluation_percentages*100)-100
 if statistic == "mean":
-    ax.errorbar(BP_evaluation_percentages[start:end], BP_evaluation_statistic[start:end], BP_evaluation_std[start:end], capsize=5, label = "BP")
+    ax.errorbar(CfC_evaluation_percentages[start:end], CfC_evaluation_statistic[start:end], CfC_evaluation_std[start:end], capsize=5, label = "CfC")
 elif statistic == "median":
-    ax.plot(BP_evaluation_percentages[start:end], BP_evaluation_statistic[start:end], label = "BP")
+    ax.plot(CfC_evaluation_percentages[start:end], CfC_evaluation_statistic[start:end], label = "CfC")
 
-BPRANGE_evaluation_statistic = np.load(f"../BP_A2C/evaluation_results/BP_A2C_RNN_range0.91.1_10models/{modified_parameter}/{statistic}s.npy")
-BPRANGE_evaluation_std = np.load(f"../BP_A2C/evaluation_results/BP_A2C_RNN_range0.91.1_10models/{modified_parameter}/stddevs.npy")
-BPRANGE_evaluation_percentages = np.load(f"../BP_A2C/evaluation_results/BP_A2C_RNN_range0.91.1_10models/{modified_parameter}/percentages.npy")
-BPRANGE_evaluation_percentages = (BPRANGE_evaluation_percentages*100)-100
-if statistic == "mean":
-    ax.errorbar(BPRANGE_evaluation_percentages[start:end], BPRANGE_evaluation_statistic[start:end], BPRANGE_evaluation_std[start:end], capsize=5, label = "BP_0.9_1.1")
-elif statistic == "median":
-    ax.plot(BPRANGE_evaluation_percentages[start:end], BPRANGE_evaluation_statistic[start:end], label = "BP_0.9_1.1")
 
-BPRANGE2_evaluation_statistic = np.load(f"../BP_A2C/evaluation_results/BP_A2C_RNN_range0.82.0_10models/{modified_parameter}/{statistic}s.npy")
-BPRANGE2_evaluation_std = np.load(f"../BP_A2C/evaluation_results/BP_A2C_RNN_range0.82.0_10models/{modified_parameter}/stddevs.npy")
-BPRANGE2_evaluation_percentages = np.load(f"../BP_A2C/evaluation_results/BP_A2C_RNN_range0.82.0_10models/{modified_parameter}/percentages.npy")
-BPRANGE2_evaluation_percentages = (BPRANGE2_evaluation_percentages*100)-100
-if statistic == "mean":
-    ax.errorbar(BPRANGE2_evaluation_percentages[start:end], BPRANGE2_evaluation_statistic[start:end], BPRANGE2_evaluation_std[start:end], capsize=5, label = "BP_0.8_2.0")
-elif statistic == "median":
-    ax.plot(BPRANGE2_evaluation_percentages[start:end], BPRANGE2_evaluation_statistic[start:end], label = "BP_0.8_2.0")
+# BP_evaluation_statistic = np.load(f"../BP_A2C/evaluation_results/BP_A2C_RNN_10models/{modified_parameter}/{statistic}s.npy")
+# BP_evaluation_std = np.load(f"../BP_A2C/evaluation_results/BP_A2C_RNN_10models/{modified_parameter}/stddevs.npy")
+# BP_evaluation_percentages = np.load(f"../BP_A2C/evaluation_results/BP_A2C_RNN_10models/{modified_parameter}/percentages.npy")
+# BP_evaluation_percentages = (BP_evaluation_percentages*100)-100
+# if statistic == "mean":
+#     ax.errorbar(BP_evaluation_percentages[start:end], BP_evaluation_statistic[start:end], BP_evaluation_std[start:end], capsize=5, label = "BP")
+# elif statistic == "median":
+#     ax.plot(BP_evaluation_percentages[start:end], BP_evaluation_statistic[start:end], label = "BP")
 
-BPRANGE3_evaluation_statistic = np.load(f"../BP_A2C/evaluation_results/BP_A2C_RNN_range0.73.0_10models/{modified_parameter}/{statistic}s.npy")
-BPRANGE3_evaluation_std = np.load(f"../BP_A2C/evaluation_results/BP_A2C_RNN_range0.73.0_10models/{modified_parameter}/stddevs.npy")
-BPRANGE3_evaluation_percentages = np.load(f"../BP_A2C/evaluation_results/BP_A2C_RNN_range0.73.0_10models/{modified_parameter}/percentages.npy")
-BPRANGE3_evaluation_percentages = (BPRANGE3_evaluation_percentages*100)-100
-if statistic == "mean":
-    ax.errorbar(BPRANGE3_evaluation_percentages[start:end], BPRANGE3_evaluation_statistic[start:end], BPRANGE3_evaluation_std[start:end], capsize=5, label = "BP_0.7_3.0")
-elif statistic == "median":
-    ax.plot(BPRANGE3_evaluation_percentages[start:end], BPRANGE3_evaluation_statistic[start:end], label = "BP_0.7_3.0")
+# BPRANGE_evaluation_statistic = np.load(f"../BP_A2C/evaluation_results/BP_A2C_RNN_range0.91.1_10models/{modified_parameter}/{statistic}s.npy")
+# BPRANGE_evaluation_std = np.load(f"../BP_A2C/evaluation_results/BP_A2C_RNN_range0.91.1_10models/{modified_parameter}/stddevs.npy")
+# BPRANGE_evaluation_percentages = np.load(f"../BP_A2C/evaluation_results/BP_A2C_RNN_range0.91.1_10models/{modified_parameter}/percentages.npy")
+# BPRANGE_evaluation_percentages = (BPRANGE_evaluation_percentages*100)-100
+# if statistic == "mean":
+#     ax.errorbar(BPRANGE_evaluation_percentages[start:end], BPRANGE_evaluation_statistic[start:end], BPRANGE_evaluation_std[start:end], capsize=5, label = "BP_0.9_1.1")
+# elif statistic == "median":
+#     ax.plot(BPRANGE_evaluation_percentages[start:end], BPRANGE_evaluation_statistic[start:end], label = "BP_0.9_1.1")
 
-BPRANGE4_evaluation_statistic = np.load(f"../BP_A2C/evaluation_results/BP_A2C_RNN_range0.79.0_10models/{modified_parameter}/{statistic}s.npy")
-BPRANGE4_evaluation_std = np.load(f"../BP_A2C/evaluation_results/BP_A2C_RNN_range0.79.0_10models/{modified_parameter}/stddevs.npy")
-BPRANGE4_evaluation_percentages = np.load(f"../BP_A2C/evaluation_results/BP_A2C_RNN_range0.79.0_10models/{modified_parameter}/percentages.npy")
-BPRANGE4_evaluation_percentages = (BPRANGE4_evaluation_percentages*100)-100
-if statistic == "mean":
-    ax.errorbar(BPRANGE4_evaluation_percentages[start:end], BPRANGE4_evaluation_statistic[start:end], BPRANGE4_evaluation_std[start:end], capsize=5, label = "BP_0.7_9.0")
-elif statistic == "median":
-    ax.plot(BPRANGE4_evaluation_percentages[start:end], BPRANGE4_evaluation_statistic[start:end], label = "BP_0.7_9.0")
+# BPRANGE2_evaluation_statistic = np.load(f"../BP_A2C/evaluation_results/BP_A2C_RNN_range0.82.0_10models/{modified_parameter}/{statistic}s.npy")
+# BPRANGE2_evaluation_std = np.load(f"../BP_A2C/evaluation_results/BP_A2C_RNN_range0.82.0_10models/{modified_parameter}/stddevs.npy")
+# BPRANGE2_evaluation_percentages = np.load(f"../BP_A2C/evaluation_results/BP_A2C_RNN_range0.82.0_10models/{modified_parameter}/percentages.npy")
+# BPRANGE2_evaluation_percentages = (BPRANGE2_evaluation_percentages*100)-100
+# if statistic == "mean":
+#     ax.errorbar(BPRANGE2_evaluation_percentages[start:end], BPRANGE2_evaluation_statistic[start:end], BPRANGE2_evaluation_std[start:end], capsize=5, label = "BP_0.8_2.0")
+# elif statistic == "median":
+#     ax.plot(BPRANGE2_evaluation_percentages[start:end], BPRANGE2_evaluation_statistic[start:end], label = "BP_0.8_2.0")
+
+# BPRANGE3_evaluation_statistic = np.load(f"../BP_A2C/evaluation_results/BP_A2C_RNN_range0.73.0_10models/{modified_parameter}/{statistic}s.npy")
+# BPRANGE3_evaluation_std = np.load(f"../BP_A2C/evaluation_results/BP_A2C_RNN_range0.73.0_10models/{modified_parameter}/stddevs.npy")
+# BPRANGE3_evaluation_percentages = np.load(f"../BP_A2C/evaluation_results/BP_A2C_RNN_range0.73.0_10models/{modified_parameter}/percentages.npy")
+# BPRANGE3_evaluation_percentages = (BPRANGE3_evaluation_percentages*100)-100
+# if statistic == "mean":
+#     ax.errorbar(BPRANGE3_evaluation_percentages[start:end], BPRANGE3_evaluation_statistic[start:end], BPRANGE3_evaluation_std[start:end], capsize=5, label = "BP_0.7_3.0")
+# elif statistic == "median":
+#     ax.plot(BPRANGE3_evaluation_percentages[start:end], BPRANGE3_evaluation_statistic[start:end], label = "BP_0.7_3.0")
+
+# BPRANGE4_evaluation_statistic = np.load(f"../BP_A2C/evaluation_results/BP_A2C_RNN_range0.79.0_10models/{modified_parameter}/{statistic}s.npy")
+# BPRANGE4_evaluation_std = np.load(f"../BP_A2C/evaluation_results/BP_A2C_RNN_range0.79.0_10models/{modified_parameter}/stddevs.npy")
+# BPRANGE4_evaluation_percentages = np.load(f"../BP_A2C/evaluation_results/BP_A2C_RNN_range0.79.0_10models/{modified_parameter}/percentages.npy")
+# BPRANGE4_evaluation_percentages = (BPRANGE4_evaluation_percentages*100)-100
+# if statistic == "mean":
+#     ax.errorbar(BPRANGE4_evaluation_percentages[start:end], BPRANGE4_evaluation_statistic[start:end], BPRANGE4_evaluation_std[start:end], capsize=5, label = "BP_0.7_9.0")
+# elif statistic == "median":
+#     ax.plot(BPRANGE4_evaluation_percentages[start:end], BPRANGE4_evaluation_statistic[start:end], label = "BP_0.7_9.0")
 
 
 # Standard_evaluation_statistic = np.load(f"../BP_A2C/evaluation_results/Standard_A2C_RNN_10models/{modified_parameter}/{statistic}s.npy")
@@ -118,21 +128,21 @@ elif statistic == "median":
 # elif statistic == "median":
 #     ax.plot(MLPRANGE4_evaluation_percentages[start:end], MLPRANGE4_evaluation_statistic[start:end], label = "MLP_0.6_4.0")
 
-MLPRANGE5_evaluation_statistic = np.load(f"../BP_A2C/evaluation_results/Standard_A2C_MLP_range0.79.0_10models/{modified_parameter}/{statistic}s.npy")
-MLPRANGE5_evaluation_std = np.load(f"../BP_A2C/evaluation_results/Standard_A2C_MLP_range0.79.0_10models/{modified_parameter}/stddevs.npy")
-MLPRANGE5_evaluation_percentages = np.load(f"../BP_A2C/evaluation_results/Standard_A2C_MLP_range0.79.0_10models/{modified_parameter}/percentages.npy")
-MLPRANGE5_evaluation_percentages = (MLPRANGE5_evaluation_percentages*100)-100
-if statistic == "mean":
-    ax.errorbar(MLPRANGE5_evaluation_percentages[start:end], MLPRANGE5_evaluation_statistic[start:end], MLPRANGE5_evaluation_std[start:end], capsize=5, label = "MLP_0.7_9.0")
-elif statistic == "median":
-    ax.plot(MLPRANGE5_evaluation_percentages[start:end], MLPRANGE5_evaluation_statistic[start:end], label = "MLP_0.7_9.0")
+# MLPRANGE5_evaluation_statistic = np.load(f"../BP_A2C/evaluation_results/Standard_A2C_MLP_range0.79.0_10models/{modified_parameter}/{statistic}s.npy")
+# MLPRANGE5_evaluation_std = np.load(f"../BP_A2C/evaluation_results/Standard_A2C_MLP_range0.79.0_10models/{modified_parameter}/stddevs.npy")
+# MLPRANGE5_evaluation_percentages = np.load(f"../BP_A2C/evaluation_results/Standard_A2C_MLP_range0.79.0_10models/{modified_parameter}/percentages.npy")
+# MLPRANGE5_evaluation_percentages = (MLPRANGE5_evaluation_percentages*100)-100
+# if statistic == "mean":
+#     ax.errorbar(MLPRANGE5_evaluation_percentages[start:end], MLPRANGE5_evaluation_statistic[start:end], MLPRANGE5_evaluation_std[start:end], capsize=5, label = "MLP_0.7_9.0")
+# elif statistic == "median":
+#     ax.plot(MLPRANGE5_evaluation_percentages[start:end], MLPRANGE5_evaluation_statistic[start:end], label = "MLP_0.7_9.0")
 
 ax.grid(True)
 ax.set_ylim(0, 250)
 ax.set_xlabel(f"{modified_parameter} increase (%)")
 ax.set_ylabel(f"{statistic} average reward")
 ax.set_title(f"{statistic} average evaluation reward in modified environments ({modified_parameter})")
-ax.set_xticks(BP_evaluation_percentages[start:end])
+ax.set_xticks(CfC_evaluation_percentages[start:end])
 ax.legend()
 
 

f355bcf9b45fc66cd83cdd110baeb5817069a484
Luuk van Keeken <luukvankeeken24@gmail.com>
2024-02-22 13:37:00 +0100

    Updated habrok test.

--- habrok_test.py
+++ habrok_test.py
@@ -25,3 +25,16 @@ neuromod_network_dims = [3, 256, 128, num_neurons]
 
 agent_net = CfC_Network(4, num_neurons, 2, seed, mode = mode, wiring = wiring, neuromod_network_dims=neuromod_network_dims).to(device)
 
+hidden_state = None
+
+priv = torch.tensor([0.5, 0.1, 10.0], dtype=torch.float32).unsqueeze(0).to(device)
+
+state = env.reset()
+state = torch.from_numpy(state)
+state = state.unsqueeze(0).to(device)
+
+policy_output, value, hidden_state = agent_net((state.float(), priv), hidden_state)
+
+print(policy_output)
+print(value)
+print(hidden_state)

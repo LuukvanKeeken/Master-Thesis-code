5652be02dac5052ce271065c353fe831017ab55a
Luuk van Keeken <luukvankeeken24@gmail.com>
2024-03-23 00:53:08 +0100

    Setting up grid search again.

--- all_jobscripts/jobscript_adaptmod_48neurons_0.0005lr_[3, 128, 80]_identity.sh
+++ all_jobscripts/jobscript_adaptmod_48neurons_0.0005lr_[3, 128, 80]_identity.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-    #SBATCH --time=10:00:00
+#SBATCH --time=10:00:00
 
-    module purge
-    module load Python/3.8.16-GCCcore-11.2.0
+module purge
+module load Python/3.8.16-GCCcore-11.2.0
 
-    source $HOME/venvs/LTC/bin/activate
+source $HOME/venvs/LTC/bin/activate
 
-    python3 -m Master_Thesis_Code.training_LTC_neuromod_habrok --num_neurons 48 --learning_rate 0.0005 --result_id 405 --neuromod_network_dims 3 128 80 --encoder_output_activation identity
+python3 -m Master_Thesis_Code.training_LTC_neuromod_habrok --num_neurons 48 --learning_rate 0.0005 --result_id 426 --neuromod_network_dims 3 128 80 --encoder_output_activation identity
 
-    deactivate
+deactivate

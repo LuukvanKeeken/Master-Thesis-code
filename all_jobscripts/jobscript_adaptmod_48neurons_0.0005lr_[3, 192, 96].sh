7fa2bc2bd29d36fe874ea7e5087603c0f2b1818a
Luuk van Keeken <luukvankeeken24@gmail.com>
2024-03-25 13:30:08 +0100

    Grid search of old code with torch.no_grad turned on, and Relu as encoder output activation.

---
+++ all_jobscripts/jobscript_adaptmod_48neurons_0.0005lr_[3, 192, 96].sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --time=10:00:00
+
+module purge
+module load Python/3.8.16-GCCcore-11.2.0
+
+source $HOME/venvs/LTC/bin/activate
+
+python3 -m Master_Thesis_Code.training_LTC_neuromod_habrok_old --num_neurons 48 --learning_rate 0.0005 --result_id 609 --neuromod_network_dims 3 192 96
+
+deactivate

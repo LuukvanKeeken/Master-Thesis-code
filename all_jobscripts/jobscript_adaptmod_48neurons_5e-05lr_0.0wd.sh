82115dfca49b60ff64758998edca160926ec5ffa
Luuk van Keeken <luukvankeeken24@gmail.com>
2024-03-27 23:43:14 +0100

    Again.

--- all_jobscripts/jobscript_adaptmod_48neurons_5e-05lr_0.0wd.sh
+++ all_jobscripts/jobscript_adaptmod_48neurons_5e-05lr_0.0wd.sh
@@ -6,6 +6,6 @@ module load Python/3.8.16-GCCcore-11.2.0
 
 source $HOME/venvs/LTC/bin/activate
 
-python3 -m Master_Thesis_Code.training_adaptation_module_batched --num_neurons_adaptation 48 --lr_adapt_mod 5e-05 --result_id 669 --wd_adapt_mod 0.0
+python3 -m Master_Thesis_Code.training_adaptation_module_batched --num_neurons_adaptation 48 --lr_adapt_mod 5e-05 --wd_adapt_mod 0.0 --result_id 669 
 
 deactivate
--- all_jobscripts/jobscript_adaptmod_64neurons_1e-05lr_0.01wd.sh
+++ all_jobscripts/jobscript_adaptmod_64neurons_1e-05lr_0.01wd.sh
@@ -6,6 +6,6 @@ module load Python/3.8.16-GCCcore-11.2.0
 
 source $HOME/venvs/LTC/bin/activate
 
-python3 -m Master_Thesis_Code.training_adaptation_module_batched --num_neurons_adaptation 64 --lr_adapt_mod 1e-05 --result_id 659 --wd_adapt_mod 0.01
+python3 -m Master_Thesis_Code.training_adaptation_module_batched --num_neurons_adaptation 64 --lr_adapt_mod 1e-05 --wd_adapt_mod 0.01 --result_id 659 
 
 deactivate

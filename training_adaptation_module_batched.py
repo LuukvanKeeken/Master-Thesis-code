ae9abd317360f44164be4868f703a4d8f1b763ff
Luuk van Keeken <luukvankeeken24@gmail.com>
2024-04-09 15:58:14 +0200

    Removed a bunch of unused models. Also setting up grid search for adaptmod for best BP relu encoder agent.

--- training_adaptation_module_batched.py
+++ training_adaptation_module_batched.py
@@ -406,7 +406,7 @@ def train_adaptation_module(env, num_parallel_envs, batch_size, num_training_epi
 
 
 parser = argparse.ArgumentParser(description='Train adaptation module for neuromodulated CfC')
-parser.add_argument('--neuron_type', type=str, default='CfC', help='Type of neuron to train')
+parser.add_argument('--neuron_type', type=str, default='BP', help='Type of neuron to train')
 parser.add_argument('--device', type=str, default='cpu', help='Device to train on')
 parser.add_argument('--state_dims', type=int, default=4, help='Number of state dimensions')
 parser.add_argument('--action_dims', type=int, default=1, help='Number of action dimensions')
@@ -416,7 +416,7 @@ parser.add_argument('--num_actions', type=int, default=2, help='Number of action
 parser.add_argument('--seed', type=int, default=5)
 parser.add_argument('--mode', type=str, default='neuromodulated', help='Mode of the CfC network')
 parser.add_argument('--wiring', type=str, default='None', help='Wiring of the CfC network')
-parser.add_argument('--neuromod_network_dims', type=int, nargs='+', default = [3, 256, 128], help='Dimensions of the neuromodulation network, without output layer')
+parser.add_argument('--neuromod_network_dims', type=int, nargs='+', default = [3, 192, 96], help='Dimensions of the neuromodulation network, without output layer')
 parser.add_argument('--num_training_eps', type=int, default=10000, help="Number of episodes to train the adaptation module")
 parser.add_argument('--env_name', type=str, default="CartPole-v0", help="Gym RL environment name")
 parser.add_argument('--lr_adapt_mod', type=float, default=0.0005, help="Learning rate of the adaptation module")
@@ -429,8 +429,8 @@ parser.add_argument('--adapt_mod_type', type=str, default='StandardRNN', help='T
 parser.add_argument('--result_id', type=int, default=-1, help='ID of the result')
 parser.add_argument('--batch_size', type=int, default=50, help='Batch size for training the adaptation module')
 parser.add_argument('--num_parallel_envs', type=int, default=10, help='Number of parallel environments to train the adaptation module')
-parser.add_argument('--encoder_hidden_activation', type=str, default='tanh', help='Activation function for the encoder hidden layers')
-parser.add_argument('--encoder_output_activation', type=str, default='tanh', help='Activation function for the encoder output layer')
+parser.add_argument('--encoder_hidden_activation', type=str, default='relu', help='Activation function for the encoder hidden layers')
+parser.add_argument('--encoder_output_activation', type=str, default='relu', help='Activation function for the encoder output layer')
 
 
 args = parser.parse_args()
@@ -489,7 +489,7 @@ else:
     raise NotImplementedError
 evaluation_seeds = np.load('Master_Thesis_Code/rstdp_cartpole_stuff/seeds/evaluation_seeds.npy')
 
-phase_1_dir = "CfC_a2c_result_2072_202448_learningrate_0.0001_numneurons_32_encoutact_tanh_neuromod_network_dims_3_256_128_32"
+phase_1_dir = "BP_a2c_result_2169_202448_learningrate_0.0005_numneurons_32_encoutact_relu_neuromod_network_dims_3_192_96_32"
 
 if result_id == -1:
     dirs = os.listdir(f'Master_Thesis_Code/{top_dir}/adaptation_module/training_results/')

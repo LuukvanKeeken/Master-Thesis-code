00c1788e6ffea28548ea6b6f43600d0eb8fb3e14
Luuk van Keeken <luukvankeeken24@gmail.com>
2024-04-12 13:10:07 +0200

    Setting up grid search for adaptmods for best tanh only_neuromod CfC encoder agent.

--- training_adaptation_module_batched.py
+++ training_adaptation_module_batched.py
@@ -410,7 +410,7 @@ parser.add_argument('--neuron_type', type=str, default='CfC', help='Type of neur
 parser.add_argument('--device', type=str, default='cpu', help='Device to train on')
 parser.add_argument('--state_dims', type=int, default=4, help='Number of state dimensions')
 parser.add_argument('--action_dims', type=int, default=1, help='Number of action dimensions')
-parser.add_argument('--num_neurons_policy', type=int, default=64, help='Number of neurons in the policy network')
+parser.add_argument('--num_neurons_policy', type=int, default=32, help='Number of neurons in the policy network')
 parser.add_argument('--num_neurons_adaptation', type=int, default=64, help='Number of neurons in the adaptation module')
 parser.add_argument('--num_actions', type=int, default=2, help='Number of actions')
 parser.add_argument('--seed', type=int, default=5)
@@ -429,8 +429,8 @@ parser.add_argument('--adapt_mod_type', type=str, default='StandardRNN', help='T
 parser.add_argument('--result_id', type=int, default=-1, help='ID of the result')
 parser.add_argument('--batch_size', type=int, default=50, help='Batch size for training the adaptation module')
 parser.add_argument('--num_parallel_envs', type=int, default=10, help='Number of parallel environments to train the adaptation module')
-parser.add_argument('--encoder_hidden_activation', type=str, default='relu', help='Activation function for the encoder hidden layers')
-parser.add_argument('--encoder_output_activation', type=str, default='relu', help='Activation function for the encoder output layer')
+parser.add_argument('--encoder_hidden_activation', type=str, default='tanh', help='Activation function for the encoder hidden layers')
+parser.add_argument('--encoder_output_activation', type=str, default='tanh', help='Activation function for the encoder output layer')
 
 
 args = parser.parse_args()
@@ -489,7 +489,7 @@ else:
     raise NotImplementedError
 evaluation_seeds = np.load('Master_Thesis_Code/rstdp_cartpole_stuff/seeds/evaluation_seeds.npy')
 
-phase_1_dir = "CfC_a2c_result_5015_2024411_learningrate_0.0001_numneurons_64_encoutact_relu_neuromod_network_dims_3_192_96_64"
+phase_1_dir = "CfC_a2c_result_5068_2024411_learningrate_0.0005_numneurons_32_encoutact_tanh_neuromod_network_dims_3_192_96_32"
 
 if result_id == -1:
     dirs = os.listdir(f'Master_Thesis_Code/{top_dir}/adaptation_module/training_results/')

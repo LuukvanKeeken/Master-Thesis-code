375336c199e88c82742988bee6b201da72131cb3
Luuk van Keeken <luukvankeeken24@gmail.com>
2024-04-13 17:58:04 +0200

    Grid search for pure CfC models trained and validated on the testing ranges, to get an idea of the upper limit.

--- training_LTC_habrok.py
+++ training_LTC_habrok.py
@@ -100,6 +100,10 @@ def randomize_env_params(env, randomization_params):
         elif isinstance(randomization_params[i], tuple):
             low = params[i]*randomization_params[i][0]
             high = params[i]*randomization_params[i][1]
+        elif isinstance(randomization_params[i], list):
+            param_range = random.choice(randomization_params[i])
+            low = param_range[0]
+            high = param_range[1]
             
         params[i] = np.random.uniform(low, high)
 
@@ -277,6 +281,45 @@ def train_agent(env, num_training_episodes, max_steps, agent_net, num_outputs, e
                     np.random.set_state(current_np_seed)
                     random.setstate(current_r_seed)
 
+                elif ((selection_method == "testing_ranges_selection") and (episode % evaluate_every == 0)):
+                    validation_ranges = [[(0.1, 0.55), (10.5, 20.0)], [(5.0, 13.0)], [(0.2, 0.6), (3.5, 6.0)]]
+
+                    eps_per_setting = 1
+                    evaluation_performance = 0
+                    total_eval_eps = 10
+                    current_np_seed = np.random.get_state()
+                    current_r_seed = random.getstate()
+                    for i in range(total_eval_eps):
+                        np.random.seed((evaluation_seeds[i+eps_per_setting-1] + seed)%(2**32))
+                        random.seed((evaluation_seeds[i+eps_per_setting-1] + seed)%(2**32))
+                        pole_length_range = random.choice(validation_ranges[0])
+                        pole_length_mod = np.random.uniform(pole_length_range[0], pole_length_range[1])
+                        pole_mass_mod = np.random.uniform(validation_ranges[1][0][0], validation_ranges[1][0][1])
+                        force_mag_range = random.choice(validation_ranges[2])
+                        force_mag_mod = np.random.uniform(force_mag_range[0], force_mag_range[1])
+                        evaluation_performance += np.mean(evaluate_agent_all_params(agent_net, env_name, eps_per_setting, evaluation_seeds[i+eps_per_setting:], pole_length_mod, pole_mass_mod, force_mag_mod))
+
+                    evaluation_performance /= total_eval_eps
+                    print(f"Episode {episode}\tAverage evaluation: {evaluation_performance}")
+
+                    if evaluation_performance >= best_average:
+                        best_average = evaluation_performance
+                        best_average_after = episode
+                        torch.save(agent_net.state_dict(),
+                                       result_dir + f'/checkpoint_{neuron_type}_A2C_{i_run}.pt')
+                        
+                    if best_average == max_reward:
+                        print(f'Best {selection_method}: ', best_average, ' reached at episode ',
+                        best_average_after, f'. Model saved in folder {result_dir}')
+                        return smoothed_scores, scores, best_average, best_average_after
+
+                    np.random.set_state(current_np_seed)
+                    random.setstate(current_r_seed)
+
+
+
+
+
                 elif (selection_method == "100 episode average"):
                     scores_window.append(score)
                     scores.append(score)
@@ -328,10 +371,10 @@ parser = argparse.ArgumentParser(description='Train an A2C agent on the CartPole
 parser.add_argument('--num_neurons', type=int, default=64, help='Number of neurons in the hidden layer')
 parser.add_argument('--neuron_type', type=str, default='CfC', help='Type of neuron, either "LTC" or "CfC"')
 parser.add_argument('--learning_rate', type=float, default=0.00005, help='Learning rate for the agent')
-parser.add_argument('--training_method', type=str, default = "quarter_range", help='Method to train the agent')
-parser.add_argument('--selection_method', type=str, default = "true_range_eval_all_params", help='Method to select the best model')
+parser.add_argument('--training_method', type=str, default = "testing_range", help='Method to train the agent')
+parser.add_argument('--selection_method', type=str, default = "testing_ranges_selection", help='Method to select the best model')
 parser.add_argument('--seed', type=int, default=5, help='Seed for the random number generator')
-parser.add_argument('--result_id', type=int, default=10999, help='ID for the result directory')
+parser.add_argument('--result_id', type=int, default=-1, help='ID for the result directory')
 
 args = parser.parse_args()
 
@@ -364,6 +407,8 @@ elif training_method == "randfactor":
     randomization_params = 3*[factor]
 elif training_method == "original":
     randomization_params = None
+elif training_method == "testing_range":
+    randomization_params = [[(0.1, 0.55), (10.5, 20.0)], [(5.0, 13.0)], [(0.2, 0.6), (3.5, 6.0)]]
 
 if result_id == -1:
     dirs = os.listdir('Master_Thesis_Code/LTC_A2C/training_results/')

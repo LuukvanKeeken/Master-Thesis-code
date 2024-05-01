aa3d12f405581a47554076aa3f447a0d10a1f002
Luuk van Keeken <luukvankeeken24@gmail.com>
2024-05-01 21:10:15 +0200

    Setting up a test for running multiple batches of jobscripts.

---
+++ all_jobscripts/big_jobscript_script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+chmod +x 1_CfC_range_48/all_jobs_CfC_range_48.sh
+chmod +x 1_CfC_range_48/status_checker_CfC_range_48.sh
+
+
+./1_CfC_range_48/all_jobs_CfC_range_48.sh
+./1_CfC_range_48/status_checker_CfC_range_48.sh &

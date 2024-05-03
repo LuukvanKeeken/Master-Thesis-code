25fec11d276d5ad68a0faf8ca0f3b93dd30c419e
Luuk van Keeken <luukvankeeken24@gmail.com>
2024-05-03 08:48:47 +0200

    Setting up getting results.

--- get_grid_search_results.py
+++ get_grid_search_results.py
@@ -1,28 +1,73 @@
+import os
 import numpy as np
 
-nums_neurons = [64, 48, 32]
-neuron_types = ['BP_RNN', 'Standard_RNN', 'Standard_MLP']
-learning_rates = [0.001, 0.0005, 0.0001, 0.00005, 0.00001]
+nums_neurons = [48]
+types = ["CfC"]
+learning_rates = [0.001, 0.0001, 0.00001,  0.000001]
+entropy_coefs = [0.0, 0.0001, 0.01, 1.0]
+value_coefs = [0.0001, 0.01, 0.5, 1.0]
+
+
+training_method = "quarter_range"
 num_models = 10
 all_results = []
-results_id = 21
-date = 202434
-
-for neuron_type in neuron_types:
+results_id = 30500
+date = 2024425
+num_train_eps = 40000
+missing_ids = []
+fine_ids = []
+for neuron_type in types:
     for num_neurons in nums_neurons:
         for learning_rate in learning_rates:
+            for entropy_coef in entropy_coefs:
+                for value_coef in value_coefs:
 
-            results_dir = f'{neuron_type}_a2c_result_{results_id}_{date}_entropycoef_0.01_valuepredcoef_0.1_batchsize_1_maxsteps_200_maxgradnorm_4.0_gammaR_0.99_learningrate_{learning_rate}_numtrainepisodes_20000_selectionmethod_range_evaluation_all_params_trainingmethod_original'
+                    if neuron_type == "CfC" or neuron_type == "LTC":
+                        top_dir = "LTC_A2C"
+                    else:
+                        top_dir = "BP_A2C"    
 
-            with open(f"Master_Thesis_Code/BP_A2C/training_results/{results_dir}/best_average_after.txt", "r") as file:
-                for i in range(num_models):
-                    file.readline()
 
-                last_line = file.readline()
-                last_line = last_line.split(" ")
-                all_results.append((results_id, neuron_type, num_neurons, learning_rate, last_line[9], last_line[12]))
-                
+                    if neuron_type == "CfC":
+                        results_dir = f'CfC_a2c_result_{results_id}_{date}_learningrate_{learning_rate}_selectiomethod_true_range_eval_all_params_trainingmethod_{training_method}_numneurons_{num_neurons}_mode_pure'
+                    elif neuron_type == "LTC":
+                        # results_dir = f'LTC_a2c_result_{results_id}_{date}_learningrate_{learning_rate}_selectiomethod_range_evaluation_all_params_gamma_0.99_trainingmethod_standard_numneurons_{num_neurons}_tausysextraction_True'
+                        results_dir = f'LTC_a2c_result_{results_id}_{date}_learningrate_{learning_rate}_selectiomethod_true_range_eval_all_params_trainingmethod_original_numneurons_{num_neurons}_tausysextraction_True'
+                    elif neuron_type == "BP":
+                        results_dir = f'BP_RNN_a2c_result_{results_id}_{date}_entropycoef_{entropy_coef}_valuepredcoef_{value_coef}_learningrate_{learning_rate}_numtrainepisodes_{num_train_eps}_selectionmethod_true_range_eval_all_params_trainingmethod_quarter_range_numneurons_{num_neurons}'
+                    
+                    elif neuron_type == "StandardRNN":
+                        results_dir = f'Standard_RNN_a2c_result_{results_id}_{date}_entropycoef_{entropy_coef}_valuepredcoef_{value_coef}_learningrate_{learning_rate}_numtrainepisodes_{num_train_eps}_selectionmethod_true_range_eval_all_params_trainingmethod_quarter_range_numneurons_{num_neurons}'
+                    
+                    elif neuron_type == "StandardMLP":
+                        results_dir = f"Standard_MLP_a2c_result_{results_id}_{date}_entropycoef_0.01_valuepredcoef_0.1_learningrate_{learning_rate}_numtrainepisodes_{num_train_eps}_selectionmethod_true_range_eval_all_params_trainingmethod_original_numneurons_{num_neurons}"
 
-            results_id += 1
+
+                    try:
+                        with open(f"Master_Thesis_Code/{top_dir}/1_CfC_range_48/{results_dir}/best_average_after.txt", "r") as file:
+                            for i in range(num_models):
+                                file.readline()
+
+                            file.readline()
+                            last_line = file.readline()
+                            last_line = last_line.split(" ")
+                            # all_results.append((results_id, neuron_type, num_neurons, learning_rate, float(last_line[3].strip(',')), float(last_line[6])))
+                            all_results.append((results_id, neuron_type, num_neurons, learning_rate, entropy_coef, value_coef, float(last_line[3].strip(',')), float(last_line[6])))
+                            fine_ids.append(results_id)
+                    except:
+                        files = os.listdir(f"Master_Thesis_Code/{top_dir}/1_CfC_range_48/{results_dir}")
+                        num_files = len(files)
+                        if num_files <= 5:
+                            print(f"Could not find {results_dir}, {num_files} models")
+                        else:
+                            print(f"Could not find {results_dir}, {num_files - 5} models")
+
+                        missing_ids.append(results_id)
+
+
+                    results_id += 1
+
 
 print(all_results)
+print(f"Missing IDs: {missing_ids}")
+print(f"Fine IDs: {fine_ids}")

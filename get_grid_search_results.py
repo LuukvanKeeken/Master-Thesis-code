9051ec99ade922a988fde43175a79a79b42648a4
Luuk van Keeken <luukvankeeken24@gmail.com>
2024-05-06 14:02:30 +0200

    Getting grid search results for LTC original 48.

--- get_grid_search_results.py
+++ get_grid_search_results.py
@@ -2,17 +2,17 @@ import os
 import numpy as np
 
 nums_neurons = [48]
-types = ["Standard_MLP"]
-learning_rates = [0.001, 0.00005, 0.000001]
+learning_rates = [0.001, 0.0001, 0.00001,  0.000001]
 entropy_coefs = [0.0, 0.0001, 0.01, 1.0]
 value_coefs = [0.0001, 0.01, 0.5, 1.0]
+types = ["LTC"]
 
 
 
 training_method = "original"
 num_models = 10
 all_results = []
-results_id = 45000
+results_id = 47000
 date = 202453
 num_train_eps = 25000
 missing_ids = []
@@ -32,8 +32,7 @@ for neuron_type in types:
                     if neuron_type == "CfC":
                         results_dir = f'CfC_a2c_result_{results_id}_{date}_learningrate_{learning_rate}_selectiomethod_true_range_eval_all_params_trainingmethod_{training_method}_numneurons_{num_neurons}_mode_pure'
                     elif neuron_type == "LTC":
-                        # results_dir = f'LTC_a2c_result_{results_id}_{date}_learningrate_{learning_rate}_selectiomethod_range_evaluation_all_params_gamma_0.99_trainingmethod_standard_numneurons_{num_neurons}_tausysextraction_True'
-                        results_dir = f'LTC_a2c_result_{results_id}_{date}_learningrate_{learning_rate}_selectiomethod_true_range_eval_all_params_trainingmethod_original_numneurons_{num_neurons}_tausysextraction_True'
+                        results_dir = f'LTC_a2c_result_{results_id}_{date}_learningrate_{learning_rate}_selectiomethod_true_range_eval_all_params_trainingmethod_{training_method}_numneurons_{num_neurons}'
                     elif neuron_type == "BP_RNN":
                         results_dir = f'BP_RNN_a2c_result_{results_id}_{date}_entropycoef_{entropy_coef}_valuepredcoef_{value_coef}_learningrate_{learning_rate}_numtrainepisodes_{num_train_eps}_selectionmethod_true_range_eval_all_params_trainingmethod_{training_method}_numneurons_{num_neurons}'
                     elif neuron_type == "Standard_RNN":
@@ -42,7 +41,7 @@ for neuron_type in types:
                         results_dir = f"Standard_MLP_a2c_result_{results_id}_{date}_entropycoef_{entropy_coef}_valuepredcoef_{value_coef}_learningrate_{learning_rate}_numtrainepisodes_{num_train_eps}_selectionmethod_true_range_eval_all_params_trainingmethod_{training_method}_numneurons_{num_neurons}"
 
                     try:
-                        with open(f"Master_Thesis_Code/{top_dir}/6_MLP_original_48/{results_dir}/best_average_after.txt", "r") as file:
+                        with open(f"Master_Thesis_Code/{top_dir}/8_LTC_original_48/{results_dir}/best_average_after.txt", "r") as file:
                             for i in range(num_models):
                                 file.readline()
 
@@ -53,7 +52,7 @@ for neuron_type in types:
                             all_results.append((results_id, neuron_type, num_neurons, learning_rate, entropy_coef, value_coef, float(last_line[3].strip(',')), float(last_line[6])))
                             fine_ids.append(results_id)
                     except:
-                        files = os.listdir(f"Master_Thesis_Code/{top_dir}/6_MLP_original_48/{results_dir}")
+                        files = os.listdir(f"Master_Thesis_Code/{top_dir}/8_LTC_original_48/{results_dir}")
                         num_files = len(files)
                         if num_files <= 5:
                             print(f"Could not find {results_dir}, {num_files} models")

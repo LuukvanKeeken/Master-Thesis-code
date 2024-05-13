6ebae9ab2e97696e83cd4f49b3af395ace53ba2f
Luuk van Keeken <luukvankeeken24@gmail.com>
2024-05-13 12:21:04 +0200

    Getting grid search results of NMBP relu and tanh 32 and 64 neurons.

--- get_grid_search_results_encoder.py
+++ get_grid_search_results_encoder.py
@@ -1,22 +1,25 @@
 import numpy as np
 
 
-nums_neurons = [48]
-learning_rates = [0.001, 0.0001, 0.00001]
+nums_neurons = [32, 64]
+learning_rates = [0.001, 0.0001]
 neuromod_nets = [[3, 256, 128], [3, 192, 96], [3, 128, 80]]
 entropy_coefs = [0.0, 0.0001, 0.01, 1.0]
-value_coefs = [0.0001, 0.01, 0.5, 1.0]
-activation_functions = ["relu"]
+value_coefs = [0.0001]
+activation_functions = ["relu", "tanh"]
+
 
 
 
 # types = ["BP", "StandardRNN", "StandardMLP"]
-
+directory = "14_NMBP_reluandtanh_32and64"
 neuron_type = "BP"
 num_models = 10
 all_results = []
-results_id = 42000
-date = 202452
+results_id = 53000
+date = 202457
+fine_ids = []
+not_fine_ids = []
 
 # for neuron_type in types:
 for num_neurons in nums_neurons:
@@ -25,7 +28,8 @@ for num_neurons in nums_neurons:
             for entropy_coef in entropy_coefs:
                 for value_coef in value_coefs:
                     for func in activation_functions:
-                        
+                        if results_id == 53043:
+                            date = 202458
 
                         if neuron_type == "CfC":
                             results_dir = f"CfC_a2c_result_{results_id}_{date}_learningrate_{learning_rate}_numneurons_{num_neurons}_encoutact_{func}_neuromod_network_dims_{'_'.join(map(str, neuromodnet))}_{num_neurons}"
@@ -43,7 +47,7 @@ for num_neurons in nums_neurons:
 
 
                         try:
-                            with open(f"Master_Thesis_Code/BP_A2C/3_NMBP_relu_48/{results_dir}/best_average_after.txt", "r") as file:
+                            with open(f"Master_Thesis_Code/BP_A2C/{directory}/{results_dir}/best_average_after.txt", "r") as file:
                                 for i in range(num_models):
                                     file.readline()
 
@@ -52,9 +56,13 @@ for num_neurons in nums_neurons:
                                 last_line = last_line.split(" ")
                                 all_results.append((results_id, num_neurons, learning_rate, neuromodnet, func, entropy_coef, value_coef, float(last_line[3].strip(',')), float(last_line[6])))
                                 # all_results.append((results_id, neuron_type, num_neurons, learning_rate, float(last_line[3].strip(',')), float(last_line[6])))
+                                fine_ids.append(results_id)
                         except:
                             print(f"Could not find {results_dir}")
+                            not_fine_ids.append(results_id)
 
                         results_id += 1
 
 print(all_results)
+print(f"Fine ids: {fine_ids}")
+print(f"Not fine ids: {not_fine_ids}")

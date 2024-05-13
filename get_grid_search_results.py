4a096b3e905f0c5425be3773696bc04a87e3dc24
Luuk van Keeken <luukvankeeken24@gmail.com>
2024-05-13 11:47:18 +0200

    Small change.

--- get_grid_search_results.py
+++ get_grid_search_results.py
@@ -13,7 +13,7 @@ directory = "13_BP_and_RNN_original_32and64"
 training_method = "original"
 num_models = 10
 all_results = []
-results_id = 52000
+results_id = 57000
 date = 202457
 num_train_eps = 25000
 missing_ids = []
@@ -37,6 +37,8 @@ for neuron_type in types:
                     elif neuron_type == "BP_RNN":
                         results_dir = f'BP_RNN_a2c_result_{results_id}_{date}_entropycoef_{entropy_coef}_valuepredcoef_{value_coef}_learningrate_{learning_rate}_numtrainepisodes_{num_train_eps}_selectionmethod_true_range_eval_all_params_trainingmethod_{training_method}_numneurons_{num_neurons}'
                     elif neuron_type == "Standard_RNN":
+                        Standard_RNN_a2c_result_52000_202457_entropycoef_0.0_valuepredcoef_0.0001_learningrate_0.001_numtrainepisodes_25000_selectionmethod_true_range_eval_all_params_trainingmethod_original_numneurons_32
+                        Standard_RNN_a2c_result_57000_202457_entropycoef_0.0_valuepredcoef_0.0001_learningrate_0.001_numtrainepisodes_25000_selectionmethod_true_range_eval_all_params_trainingmethod_original_numneurons_32
                         results_dir = f'Standard_RNN_a2c_result_{results_id}_{date}_entropycoef_{entropy_coef}_valuepredcoef_{value_coef}_learningrate_{learning_rate}_numtrainepisodes_{num_train_eps}_selectionmethod_true_range_eval_all_params_trainingmethod_{training_method}_numneurons_{num_neurons}'
                     elif neuron_type == "Standard_MLP":
                         results_dir = f"Standard_MLP_a2c_result_{results_id}_{date}_entropycoef_{entropy_coef}_valuepredcoef_{value_coef}_learningrate_{learning_rate}_numtrainepisodes_{num_train_eps}_selectionmethod_true_range_eval_all_params_trainingmethod_{training_method}_numneurons_{num_neurons}"

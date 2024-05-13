d4f6de09f75e7e7d9ece5020315ec965773f5f7f
Luuk van Keeken <luukvankeeken24@gmail.com>
2024-05-13 11:45:36 +0200

    Small change.

--- get_grid_search_results.py
+++ get_grid_search_results.py
@@ -37,7 +37,7 @@ for neuron_type in types:
                     elif neuron_type == "BP_RNN":
                         results_dir = f'BP_RNN_a2c_result_{results_id}_{date}_entropycoef_{entropy_coef}_valuepredcoef_{value_coef}_learningrate_{learning_rate}_numtrainepisodes_{num_train_eps}_selectionmethod_true_range_eval_all_params_trainingmethod_{training_method}_numneurons_{num_neurons}'
                     elif neuron_type == "Standard_RNN":
-                        results_dir = f"Standard_RNN_a2c_result_{results_id}_{date}_entropycoef_{entropy_coef}_valuepredcoef_{value_coef}_learningrate_{learning_rate}_numtrainepisodes_{num_train_eps}_selectionmethod_true_range_eval_all_params_trainingmethod_original_numneurons_{num_neurons}"
+                        results_dir = f'Standard_RNN_a2c_result_{results_id}_{date}_entropycoef_{entropy_coef}_valuepredcoef_{value_coef}_learningrate_{learning_rate}_numtrainepisodes_{num_train_eps}_selectionmethod_true_range_eval_all_params_trainingmethod_{training_method}_numneurons_{num_neurons}'
                     elif neuron_type == "Standard_MLP":
                         results_dir = f"Standard_MLP_a2c_result_{results_id}_{date}_entropycoef_{entropy_coef}_valuepredcoef_{value_coef}_learningrate_{learning_rate}_numtrainepisodes_{num_train_eps}_selectionmethod_true_range_eval_all_params_trainingmethod_{training_method}_numneurons_{num_neurons}"
 

1d3f47c6a241c3b35ebb54b0e4489ebaaa46aa13
Luuk van Keeken <luukvankeeken24@gmail.com>
2024-05-16 10:12:55 +0200

    Added average number of training episodes in output.

--- get_grid_search_results.py
+++ get_grid_search_results.py
@@ -49,7 +49,9 @@ for neuron_type in types:
                             for i in range(num_models):
                                 file.readline()
 
-                            file.readline()
+                            num_eps_line = file.readline()
+                            num_eps_line = num_eps_line.split(" ")
+                            num_eps = float(num_eps_line[3])
                             last_line = file.readline()
                             last_line = last_line.split(" ")
                             all_results.append((results_id, neuron_type, num_neurons, learning_rate, entropy_coef, value_coef, float(last_line[3].strip(',')), float(last_line[6])))

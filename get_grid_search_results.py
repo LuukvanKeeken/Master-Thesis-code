09fdcfbebddbaa87960b3a92b38904034b3bda74
Luuk van Keeken <luukvankeeken24@gmail.com>
2024-05-16 11:30:36 +0200

    Included total num of eps trained.

--- get_grid_search_results.py
+++ get_grid_search_results.py
@@ -47,14 +47,16 @@ for neuron_type in types:
                     try:
                         with open(f"Master_Thesis_Code/{top_dir}/{directory}/{results_dir}/best_average_after.txt", "r") as file:
                             for i in range(num_models):
-                                file.readline()
+                                info = file.readline()
+                                info = info.split(":")
+                                total_eps = int(info[2].split(")")[0].strip())
 
                             num_eps_line = file.readline()
                             num_eps_line = num_eps_line.split(" ")
                             num_eps = float(num_eps_line[3].strip(','))
                             last_line = file.readline()
                             last_line = last_line.split(" ")
-                            all_results.append((results_id, neuron_type, num_neurons, learning_rate, entropy_coef, value_coef, float(last_line[3].strip(',')), float(last_line[6]), num_eps))
+                            all_results.append((results_id, neuron_type, num_neurons, learning_rate, entropy_coef, value_coef, float(last_line[3].strip(',')), float(last_line[6]), num_eps, total_eps))
                             fine_ids.append(results_id)
                     except:
                         files = os.listdir(f"Master_Thesis_Code/{top_dir}/{directory}/{results_dir}")

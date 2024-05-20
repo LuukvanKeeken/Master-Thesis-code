ed60396476a8462ba098e0f296a1f378f2d5f809
Luuk van Keeken <luukvankeeken24@gmail.com>
2024-05-20 12:03:25 +0200

    Checking whether envs are run in parallel.

---
+++ parallelization_test.py
@@ -0,0 +1,21 @@
+from gym.vector import AsyncVectorEnv
+import gym
+import os
+
+# Define the environment creation function
+def make_env(env_id, seed):
+    def _f():
+        env = gym.make(env_id)
+        env.seed(seed)
+        print(f"Environment {env_id} with seed {seed} is running on PID {os.getpid()}")
+        return env
+    return _f
+
+num_envs = 100
+env_id = 'CartPole-v1'
+
+# Create a vector of environments
+envs = AsyncVectorEnv([make_env(env_id, i) for i in range(num_envs)])
+
+# Now you can interact with the environments
+observations = envs.reset()

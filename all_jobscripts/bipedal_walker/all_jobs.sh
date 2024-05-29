46129168a40c0187d4b3b4b0ec961d371ab3d64d
Luuk van Keeken <luukvankeeken24@gmail.com>
2024-05-29 22:51:09 +0200

    Corrected file paths in jobscripts.

--- all_jobscripts/bipedal_walker/all_jobs.sh
+++ all_jobscripts/bipedal_walker/all_jobs.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-sbatch 'jobscript_Standard_RNN_96neurons_0.0001lr_0.001ec_0.001vc.sh'
+sbatch 'bipedal_walker/BP_and_StandardRNN/jobscript_Standard_RNN_96neurons_0.0001lr_0.001ec_0.001vc.sh'
 sleep 3
-sbatch 'jobscript_Standard_RNN_96neurons_0.0001lr_0.001ec_1.0vc.sh'
+sbatch 'bipedal_walker/BP_and_StandardRNN/jobscript_Standard_RNN_96neurons_0.0001lr_0.001ec_1.0vc.sh'
 sleep 3
-sbatch 'jobscript_Standard_RNN_96neurons_0.0001lr_1.0ec_0.001vc.sh'
+sbatch 'bipedal_walker/BP_and_StandardRNN/jobscript_Standard_RNN_96neurons_0.0001lr_1.0ec_0.001vc.sh'
 sleep 3
-sbatch 'jobscript_Standard_RNN_96neurons_0.0001lr_1.0ec_1.0vc.sh'
+sbatch 'bipedal_walker/BP_and_StandardRNN/jobscript_Standard_RNN_96neurons_0.0001lr_1.0ec_1.0vc.sh'
 sleep 3
-sbatch 'jobscript_BP_RNN_96neurons_0.0001lr_0.001ec_0.001vc.sh'
+sbatch 'bipedal_walker/BP_and_StandardRNN/jobscript_BP_RNN_96neurons_0.0001lr_0.001ec_0.001vc.sh'
 sleep 3
-sbatch 'jobscript_BP_RNN_96neurons_0.0001lr_0.001ec_1.0vc.sh'
+sbatch 'bipedal_walker/BP_and_StandardRNN/jobscript_BP_RNN_96neurons_0.0001lr_0.001ec_1.0vc.sh'
 sleep 3
-sbatch 'jobscript_BP_RNN_96neurons_0.0001lr_1.0ec_0.001vc.sh'
+sbatch 'bipedal_walker/BP_and_StandardRNN/jobscript_BP_RNN_96neurons_0.0001lr_1.0ec_0.001vc.sh'
 sleep 3
-sbatch 'jobscript_BP_RNN_96neurons_0.0001lr_1.0ec_1.0vc.sh'
+sbatch 'bipedal_walker/BP_and_StandardRNN/jobscript_BP_RNN_96neurons_0.0001lr_1.0ec_1.0vc.sh'

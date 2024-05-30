2b274afc99dbbdbbb8a4bec749e25b88fb6714b9
Luuk van Keeken <luukvankeeken24@gmail.com>
2024-05-30 14:14:14 +0200

    Correct numbers of training episodes etc. for BP/standard RNN grid search.

--- BW_training_BP_batched.py
+++ BW_training_BP_batched.py
@@ -27,10 +27,10 @@ parser.add_argument('--output_dims', type=int, default=4, help='Number of output
 parser.add_argument('--continuous_actions', type=bool, default=True, help='Whether the environment has continuous actions')
 parser.add_argument('--entropy_coef', type=float, default=0.0001, help='Entropy coefficient for the agent')
 parser.add_argument('--value_pred_coef', type=float, default=0.0001, help='Value prediction coefficient for the agent')
-parser.add_argument('--num_training_episodes', type=int, default=30, help='Number of training episodes to run')
+parser.add_argument('--num_training_episodes', type=int, default=1000000, help='Number of training episodes to run')
 parser.add_argument('--num_evaluation_episodes', type=int, default=20, help='Number of evaluation episodes to run')
-parser.add_argument('--training_episodes_per_section', type=int, default=10, help='Number of training episodes to run per section')
-parser.add_argument('--evaluate_every', type=int, default=10, help='How often to evaluate the agent')
+parser.add_argument('--training_episodes_per_section', type=int, default=1000, help='Number of training episodes to run per section')
+parser.add_argument('--evaluate_every', type=int, default=50, help='How often to evaluate the agent')
 parser.add_argument('--batch_size', type=int, default=5, help='Batch size to use for training')
 parser.add_argument('--num_parallel_envs', type=int, default=5, help='Number of parallel environments to use')
 

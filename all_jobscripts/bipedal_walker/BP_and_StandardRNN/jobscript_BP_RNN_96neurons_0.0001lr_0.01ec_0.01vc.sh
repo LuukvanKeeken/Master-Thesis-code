6046eb90ff1e9b169fc6894f35a15242d8881f75
Luuk van Keeken <luukvankeeken24@gmail.com>
2024-06-04 16:31:38 +0200

    Fixed script name in jobscripts.

--- all_jobscripts/bipedal_walker/BP_and_StandardRNN/jobscript_BP_RNN_96neurons_0.0001lr_0.01ec_0.01vc.sh
+++ all_jobscripts/bipedal_walker/BP_and_StandardRNN/jobscript_BP_RNN_96neurons_0.0001lr_0.01ec_0.01vc.sh
@@ -7,6 +7,6 @@ module load Python/3.8.16-GCCcore-11.2.0
 
 source $HOME/venvs/LTC/bin/activate
 
-python3 -m Master_Thesis_Code.BW_training_LTC_batched --network_type BP_RNN --num_neurons 96 --learning_rate 0.0001 --result_id 400013 --entropy_coef 0.01 --value_pred_coef 0.01
+python3 -m Master_Thesis_Code.BW_training_BP_batched --network_type BP_RNN --num_neurons 96 --learning_rate 0.0001 --result_id 400013 --entropy_coef 0.01 --value_pred_coef 0.01
 
 deactivate

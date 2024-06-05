427e310414a52961284885163553aaea70d560ae
Luuk van Keeken <luukvankeeken24@gmail.com>
2024-06-05 12:05:34 +0200

    Setting up another grid search for NMCfC models with lower learning rates.

---
+++ all_jobscripts/bipedal_walker/NMCfC/jobscript_encoder_96neurons_1e-06lr_[11, 256, 128]_relu_1.0_0.01.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --time=144:00:00
+#SBATCH --mem=10000M
+
+module purge
+module load Python/3.8.16-GCCcore-11.2.0
+
+source $HOME/venvs/LTC/bin/activate
+
+python3 -m Master_Thesis_Code.BW_training_neuromod_batched --num_neurons 96 --learning_rate 1e-06 --result_id 800014 --neuromod_network_dims 11 256 128 --encoder_output_activation relu --encoder_hidden_activation relu --value_pred_coef 1.0 --entropy_coef 0.01 --neuron_type CfC
+
+deactivate
--- all_jobscripts/bipedal_walker/all_jobs_NMCfC.sh
+++ all_jobscripts/bipedal_walker/all_jobs_NMCfC.sh
@@ -1,36 +1,36 @@
 #!/bin/bash
-sbatch 'bipedal_walker/NMCfC/jobscript_encoder_96neurons_0.001lr_[11, 256, 128]_relu_0.001_0.001.sh'
+sbatch 'bipedal_walker/NMCfC/jobscript_encoder_96neurons_1e-05lr_[11, 256, 128]_relu_0.001_0.001.sh'
 sleep 3
-sbatch 'bipedal_walker/NMCfC/jobscript_encoder_96neurons_0.001lr_[11, 256, 128]_relu_0.01_0.001.sh'
+sbatch 'bipedal_walker/NMCfC/jobscript_encoder_96neurons_1e-05lr_[11, 256, 128]_relu_0.01_0.001.sh'
 sleep 3
-sbatch 'bipedal_walker/NMCfC/jobscript_encoder_96neurons_0.001lr_[11, 256, 128]_relu_1.0_0.001.sh'
+sbatch 'bipedal_walker/NMCfC/jobscript_encoder_96neurons_1e-05lr_[11, 256, 128]_relu_1.0_0.001.sh'
 sleep 3
-sbatch 'bipedal_walker/NMCfC/jobscript_encoder_96neurons_0.001lr_[11, 256, 128]_relu_0.001_0.01.sh'
+sbatch 'bipedal_walker/NMCfC/jobscript_encoder_96neurons_1e-05lr_[11, 256, 128]_relu_0.001_0.01.sh'
 sleep 3
-sbatch 'bipedal_walker/NMCfC/jobscript_encoder_96neurons_0.001lr_[11, 256, 128]_relu_0.01_0.01.sh'
+sbatch 'bipedal_walker/NMCfC/jobscript_encoder_96neurons_1e-05lr_[11, 256, 128]_relu_0.01_0.01.sh'
 sleep 3
-sbatch 'bipedal_walker/NMCfC/jobscript_encoder_96neurons_0.001lr_[11, 256, 128]_relu_1.0_0.01.sh'
+sbatch 'bipedal_walker/NMCfC/jobscript_encoder_96neurons_1e-05lr_[11, 256, 128]_relu_1.0_0.01.sh'
 sleep 3
-sbatch 'bipedal_walker/NMCfC/jobscript_encoder_96neurons_0.001lr_[11, 256, 128]_relu_0.001_1.0.sh'
+sbatch 'bipedal_walker/NMCfC/jobscript_encoder_96neurons_1e-05lr_[11, 256, 128]_relu_0.001_1.0.sh'
 sleep 3
-sbatch 'bipedal_walker/NMCfC/jobscript_encoder_96neurons_0.001lr_[11, 256, 128]_relu_0.01_1.0.sh'
+sbatch 'bipedal_walker/NMCfC/jobscript_encoder_96neurons_1e-05lr_[11, 256, 128]_relu_0.01_1.0.sh'
 sleep 3
-sbatch 'bipedal_walker/NMCfC/jobscript_encoder_96neurons_0.001lr_[11, 256, 128]_relu_1.0_1.0.sh'
+sbatch 'bipedal_walker/NMCfC/jobscript_encoder_96neurons_1e-05lr_[11, 256, 128]_relu_1.0_1.0.sh'
 sleep 3
-sbatch 'bipedal_walker/NMCfC/jobscript_encoder_96neurons_0.0001lr_[11, 256, 128]_relu_0.001_0.001.sh'
+sbatch 'bipedal_walker/NMCfC/jobscript_encoder_96neurons_1e-06lr_[11, 256, 128]_relu_0.001_0.001.sh'
 sleep 3
-sbatch 'bipedal_walker/NMCfC/jobscript_encoder_96neurons_0.0001lr_[11, 256, 128]_relu_0.01_0.001.sh'
+sbatch 'bipedal_walker/NMCfC/jobscript_encoder_96neurons_1e-06lr_[11, 256, 128]_relu_0.01_0.001.sh'
 sleep 3
-sbatch 'bipedal_walker/NMCfC/jobscript_encoder_96neurons_0.0001lr_[11, 256, 128]_relu_1.0_0.001.sh'
+sbatch 'bipedal_walker/NMCfC/jobscript_encoder_96neurons_1e-06lr_[11, 256, 128]_relu_1.0_0.001.sh'
 sleep 3
-sbatch 'bipedal_walker/NMCfC/jobscript_encoder_96neurons_0.0001lr_[11, 256, 128]_relu_0.001_0.01.sh'
+sbatch 'bipedal_walker/NMCfC/jobscript_encoder_96neurons_1e-06lr_[11, 256, 128]_relu_0.001_0.01.sh'
 sleep 3
-sbatch 'bipedal_walker/NMCfC/jobscript_encoder_96neurons_0.0001lr_[11, 256, 128]_relu_0.01_0.01.sh'
+sbatch 'bipedal_walker/NMCfC/jobscript_encoder_96neurons_1e-06lr_[11, 256, 128]_relu_0.01_0.01.sh'
 sleep 3
-sbatch 'bipedal_walker/NMCfC/jobscript_encoder_96neurons_0.0001lr_[11, 256, 128]_relu_1.0_0.01.sh'
+sbatch 'bipedal_walker/NMCfC/jobscript_encoder_96neurons_1e-06lr_[11, 256, 128]_relu_1.0_0.01.sh'
 sleep 3
-sbatch 'bipedal_walker/NMCfC/jobscript_encoder_96neurons_0.0001lr_[11, 256, 128]_relu_0.001_1.0.sh'
+sbatch 'bipedal_walker/NMCfC/jobscript_encoder_96neurons_1e-06lr_[11, 256, 128]_relu_0.001_1.0.sh'
 sleep 3
-sbatch 'bipedal_walker/NMCfC/jobscript_encoder_96neurons_0.0001lr_[11, 256, 128]_relu_0.01_1.0.sh'
+sbatch 'bipedal_walker/NMCfC/jobscript_encoder_96neurons_1e-06lr_[11, 256, 128]_relu_0.01_1.0.sh'
 sleep 3
-sbatch 'bipedal_walker/NMCfC/jobscript_encoder_96neurons_0.0001lr_[11, 256, 128]_relu_1.0_1.0.sh'
+sbatch 'bipedal_walker/NMCfC/jobscript_encoder_96neurons_1e-06lr_[11, 256, 128]_relu_1.0_1.0.sh'

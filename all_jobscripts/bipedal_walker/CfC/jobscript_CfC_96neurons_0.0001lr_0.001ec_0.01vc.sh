059b52e360ef014e228c271232c2aae20cc2b0c0
Luuk van Keeken <luukvankeeken24@gmail.com>
2024-06-06 16:56:22 +0200

    Added an additional search for CfC models with smaller learning rates.

---
+++ all_jobscripts/bipedal_walker/CfC/jobscript_CfC_96neurons_0.0001lr_0.001ec_0.01vc.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --time=144:00:00
+#SBATCH --mem=7000M
+
+module purge
+module load Python/3.8.16-GCCcore-11.2.0
+
+source $HOME/venvs/LTC/bin/activate
+
+python3 -m Master_Thesis_Code.BW_training_LTC_batched --network_type CfC --num_neurons 96 --learning_rate 0.0001 --result_id 900001 --entropy_coef 0.001 --value_pred_coef 0.01
+
+deactivate

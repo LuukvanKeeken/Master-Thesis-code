fe498840038e4d9cfbe99347711c1be4ee0ae093
Luuk van Keeken <luukvankeeken24@gmail.com>
2024-06-11 13:19:45 +0200

    Getting results for another grid search of NM CfC.

--- BW_get_grid_search_results_encoder.py
+++ BW_get_grid_search_results_encoder.py
@@ -3,11 +3,12 @@ import numpy as np
 
 
 nums_neurons = [96]
-learning_rates = [0.001, 0.0001]
+learning_rates = [0.00001, 0.000001]
 neuromod_nets = [[11, 256, 128]]
 entropy_coefs = [0.001, 0.01, 1.0]
 value_coefs = [0.001, 0.01, 1.0]
 activation_functions = ["relu"]
+
 
 
 
@@ -20,8 +21,8 @@ else:
     top_dir = "BP_A2C"
 num_models = 1
 all_results = []
-results_id = 500000
-date = 202464
+results_id = 800000
+date = 202465
 fine_ids = []
 not_fine_ids = []
 

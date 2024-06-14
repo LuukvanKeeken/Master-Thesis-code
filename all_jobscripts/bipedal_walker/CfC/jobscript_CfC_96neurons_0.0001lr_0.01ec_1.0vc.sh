9bbc6a877616eb6b1a7882f7fbfb8a03dd2d9ef4
Luuk van Keeken <luukvankeeken24@gmail.com>
2024-06-14 10:45:10 +0200

    Setting up grid search for pure CfC with averaged ranges.

--- all_jobscripts/bipedal_walker/CfC/jobscript_CfC_96neurons_0.0001lr_0.01ec_1.0vc.sh
+++ all_jobscripts/bipedal_walker/CfC/jobscript_CfC_96neurons_0.0001lr_0.01ec_1.0vc.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --time=144:00:00
+#SBATCH --time=168:00:00
 #SBATCH --mem=7000M
 
 module purge
@@ -7,6 +7,6 @@ module load Python/3.8.16-GCCcore-11.2.0
 
 source $HOME/venvs/LTC/bin/activate
 
-python3 -m Master_Thesis_Code.BW_training_LTC_batched --network_type CfC --num_neurons 96 --learning_rate 0.0001 --result_id 900005 --entropy_coef 0.01 --value_pred_coef 1.0
+python3 -m Master_Thesis_Code.BW_training_LTC_batched --network_type CfC --num_neurons 96 --learning_rate 0.0001 --result_id 2100005 --entropy_coef 0.01 --value_pred_coef 1.0
 
 deactivate

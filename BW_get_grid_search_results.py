95a1dba557eedc8a6f3785d55821afbe3e4b86b2
Luuk van Keeken <luukvankeeken24@gmail.com>
2024-06-27 15:25:53 +0200

    Getting results of BP and standard RNN grid search.

--- BW_get_grid_search_results.py
+++ BW_get_grid_search_results.py
@@ -1,20 +1,21 @@
 import os
 import numpy as np
 
-types = ["CfC"]
+types = ["Standard_RNN", "BP_RNN"]
 nums_neurons = [96]
-learning_rates = [0.0001, 0.00001]
-entropy_coefs = [0.001, 0.01, 1.0]
-value_coefs = [0.001, 0.01, 1.0]
-results_id = 900000
+learning_rates = [0.0001]
+entropy_coefs = [0.001, 0.01]
+value_coefs = [0.001, 0.01]
+results_id = 2000000
 
 
-directory = "bipedal_walker/CfC"
+
+directory = "bipedal_walker/BP_and_StandardRNN"
 training_method = "quarter_range"
 selection_method = "range"
 num_models = 1
 all_results = []
-date = 202466
+date = 2024614
 missing_ids = []
 fine_ids = []
 num_train_eps = 1000000

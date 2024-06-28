3025683f3a0c21550373266d28d64d64bd23dc21
Luuk van Keeken <luukvankeeken24@gmail.com>
2024-06-28 09:40:34 +0200

    Getting results NMCfC for averaged ranges.

--- BW_get_grid_search_results_encoder.py
+++ BW_get_grid_search_results_encoder.py
@@ -3,26 +3,25 @@ import numpy as np
 
 
 nums_neurons = [96]
-learning_rates = [0.00001, 0.000001]
+learning_rates = [0.001, 0.0001]
 neuromod_nets = [[11, 256, 128]]
-entropy_coefs = [0.001, 0.01, 1.0]
-value_coefs = [0.001, 0.01, 1.0]
-activation_functions = ["tanh"]
-
-
+entropy_coefs = [0.001, 0.01]
+value_coefs = [0.001, 0.01]
+activation_functions = ["relu", "tanh"]
+results_id = 2200000
 
 
 # types = ["BP", "StandardRNN", "StandardMLP"]
 directory = "bipedal_walker"
-neuron_type = "BP"
+neuron_type = "CfC"
 if neuron_type == "CfC":
     top_dir = "LTC_A2C"
 else:
     top_dir = "BP_A2C"
 num_models = 1
 all_results = []
-results_id = 700000
-date = 202465
+results_id = 2200000
+date = 2024614
 fine_ids = []
 not_fine_ids = []
 

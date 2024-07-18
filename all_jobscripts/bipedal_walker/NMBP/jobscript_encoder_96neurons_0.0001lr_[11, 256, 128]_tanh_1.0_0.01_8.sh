5a706f97102e7d3b31fd4e4d823dbd5634fd4805
Luuk van Keeken <luukvankeeken24@gmail.com>
2024-07-18 20:52:44 +0200

    Training 9 additional NMBP models with best hyperparameter settings.

---
+++ all_jobscripts/bipedal_walker/NMBP/jobscript_encoder_96neurons_0.0001lr_[11, 256, 128]_tanh_1.0_0.01_8.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --time=240:00:00
+#SBATCH --mem=10000M
+
+module purge
+module load Python/3.8.16-GCCcore-11.2.0
+
+source $HOME/venvs/LTC/bin/activate
+
+python3 -m Master_Thesis_Code.BW_training_neuromod_batched --num_neurons 96 --learning_rate 0.0001 --result_id 4400007 --neuromod_network_dims 11 256 128 --encoder_output_activation tanh --encoder_hidden_activation tanh --value_pred_coef 1.0 --entropy_coef 0.01 --neuron_type BP --additional_seed 8
+
+deactivate
--- all_jobscripts/bipedal_walker/all_jobs_NMBP.sh
+++ all_jobscripts/bipedal_walker/all_jobs_NMBP.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-sbatch 'bipedal_walker/NMBP/jobscript_encoder_96neurons_0.0001lr_[11, 256, 128]_tanh_0.001_0.001_1.sh'
+sbatch 'bipedal_walker/NMBP/jobscript_encoder_96neurons_0.0001lr_[11, 256, 128]_tanh_1.0_0.01_1.sh'
 sleep 3
-sbatch 'bipedal_walker/NMBP/jobscript_encoder_96neurons_0.0001lr_[11, 256, 128]_tanh_0.001_0.001_2.sh'
+sbatch 'bipedal_walker/NMBP/jobscript_encoder_96neurons_0.0001lr_[11, 256, 128]_tanh_1.0_0.01_2.sh'
 sleep 3
-sbatch 'bipedal_walker/NMBP/jobscript_encoder_96neurons_0.0001lr_[11, 256, 128]_tanh_0.001_0.001_3.sh'
+sbatch 'bipedal_walker/NMBP/jobscript_encoder_96neurons_0.0001lr_[11, 256, 128]_tanh_1.0_0.01_3.sh'
 sleep 3
-sbatch 'bipedal_walker/NMBP/jobscript_encoder_96neurons_0.0001lr_[11, 256, 128]_tanh_0.001_0.001_4.sh'
+sbatch 'bipedal_walker/NMBP/jobscript_encoder_96neurons_0.0001lr_[11, 256, 128]_tanh_1.0_0.01_4.sh'
 sleep 3
-sbatch 'bipedal_walker/NMBP/jobscript_encoder_96neurons_0.0001lr_[11, 256, 128]_tanh_0.001_0.001_5.sh'
+sbatch 'bipedal_walker/NMBP/jobscript_encoder_96neurons_0.0001lr_[11, 256, 128]_tanh_1.0_0.01_5.sh'
 sleep 3
-sbatch 'bipedal_walker/NMBP/jobscript_encoder_96neurons_0.0001lr_[11, 256, 128]_tanh_0.001_0.001_6.sh'
+sbatch 'bipedal_walker/NMBP/jobscript_encoder_96neurons_0.0001lr_[11, 256, 128]_tanh_1.0_0.01_6.sh'
 sleep 3
-sbatch 'bipedal_walker/NMBP/jobscript_encoder_96neurons_0.0001lr_[11, 256, 128]_tanh_0.001_0.001_7.sh'
+sbatch 'bipedal_walker/NMBP/jobscript_encoder_96neurons_0.0001lr_[11, 256, 128]_tanh_1.0_0.01_7.sh'
 sleep 3
-sbatch 'bipedal_walker/NMBP/jobscript_encoder_96neurons_0.0001lr_[11, 256, 128]_tanh_0.001_0.001_8.sh'
+sbatch 'bipedal_walker/NMBP/jobscript_encoder_96neurons_0.0001lr_[11, 256, 128]_tanh_1.0_0.01_8.sh'
 sleep 3
-sbatch 'bipedal_walker/NMBP/jobscript_encoder_96neurons_0.0001lr_[11, 256, 128]_tanh_0.001_0.001_9.sh'
+sbatch 'bipedal_walker/NMBP/jobscript_encoder_96neurons_0.0001lr_[11, 256, 128]_tanh_1.0_0.01_9.sh'

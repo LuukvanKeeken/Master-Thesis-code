5829c4883416b60866434374eb5e37f34db919ad
Luuk van Keeken <luukvankeeken24@gmail.com>
2024-08-19 22:16:39 +0200

    More models.

--- training_different_pole_length.py
+++ training_different_pole_length.py
@@ -12,7 +12,7 @@ parser = argparse.ArgumentParser(description='Train an A2C agent on the CartPole
 parser.add_argument('--num_neurons', type=int, default=32, help='Number of neurons in the hidden layer')
 parser.add_argument('--network_type', type=str, default='Standard_RNN', help='Type of network to use')
 parser.add_argument('--learning_rate', type=float, default=0.001, help='Learning rate for the agent')
-parser.add_argument('--num_models', type=int, default=10, help='Number of models to train')
+parser.add_argument('--num_models', type=int, default=20, help='Number of models to train')
 parser.add_argument('--selection_method', type=str, default='evaluation_diff_pole_length', help='Method to use for selecting the best model')
 parser.add_argument('--training_method', type=str, default = "original", help='Method to train the agent')
 parser.add_argument('--result_id', type=int, default=-1, help='ID to use for the results directory')
@@ -50,6 +50,7 @@ max_steps = 200
 batch_size = 1
 evaluation_seeds = np.load('Master_Thesis_Code/rstdp_cartpole_stuff/seeds/evaluation_seeds.npy')
 training_seeds = np.load('Master_Thesis_Code/rstdp_cartpole_stuff/seeds/training_seeds.npy')
+training_seeds = np.concatenate((training_seeds, training_seeds+1))
 max_reward = 200
 range_min = 0.7
 range_max = 1.3
